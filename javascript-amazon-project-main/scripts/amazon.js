7949ef195eb75f5a4947c535deafdf421d8f4678
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2025-11-15 04:00:09 +0530

    make select button interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -63,8 +63,11 @@ document.querySelector(".js-product-grid").innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
   button.addEventListener('click',()=> {
     const productId = button.dataset.productId;
-    let matchingItem;
+     const quantitySelector = Number(document.querySelector(
+        `.js-quantity-selector-${productId}`
+      ).value);
 
+    let matchingItem;
     cart.forEach((item) => {
       if(productId === item.productId){
         matchingItem = item;
@@ -72,12 +75,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
     });
 
     if(matchingItem){
-      matchingItem.quantity++;
+      matchingItem.quantity+=quantitySelector;
     }else{
       cart.push(
         {
           productId: productId,
-          quantity: 1
+          quantity: quantitySelector
         })
     }
     
