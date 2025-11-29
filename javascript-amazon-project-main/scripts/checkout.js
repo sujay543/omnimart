3437e5f0f02303289184492910d34c3286bba8b5
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2025-11-30 05:21:48 +0530

    load products from the backend

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,8 @@
 import { renderOrderSummary} from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
-renderOrderSummary();
-renderPaymentSummary();
+import { loadproducts } from "../data/products.js";
+
+loadproducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+})
