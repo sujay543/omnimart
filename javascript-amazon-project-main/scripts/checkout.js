b146a5c82c04e2b48c27328e5644e3da6f59de52
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2025-12-01 20:34:45 +0530

    learn promise and convert code to promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,7 +2,17 @@ import { renderOrderSummary} from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { loadproducts } from "../data/products.js";
 
-loadproducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
+
+Promise.all([
+  new Promise((resolve) => {
+  loadproducts(()=> {
+    resolve();
+  })
 })
+]).then(()=> {
+   renderOrderSummary();
+   renderPaymentSummary();
+})
+
+
+
